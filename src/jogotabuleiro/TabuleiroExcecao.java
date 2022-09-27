abf3e29916a945b017271c09f0117acb86cecd67
erica nogueira <ericasnogueira10@gmail.com>
2022-09-27 12:45:34 -0300

    Atualizando Projeto de xadrez

--- src/jogotabuleiro/TabuleiroExcecao.java
+++ src/jogotabuleiro/TabuleiroExcecao.java
@@ -1,5 +1,7 @@
 package jogotabuleiro;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class TabuleiroExcecao extends RuntimeException{
 
     private static final long serialVersionUID = 1L;
